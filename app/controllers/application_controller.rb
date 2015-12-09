69606b846b4509af48bb06706f4c1ef6a3bdfbd7
mwatha <mwathabwanali@gmail.com>
2015-12-09 14:54:43 +0200

    Adding bootstrap layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery #with: :exception
+
+
+  before_filter :check_with_user_has_loggedin, :except => ['login']
+
+  private
+
+  def check_with_user_has_loggedin
+    if session[:user_id].blank?
+      redirect_to '/user/login'
+    end
+  end
+
 end
